7a492cc28a1e5a8cc6984554bc125931d880573b
eric <ericabbey.8gig@gmail.com>
2018-03-10 08:06:59 +0000

    match object again

--- config/config.js
+++ config/config.js
@@ -15,4 +15,4 @@ CONFIG.db_password  = process.env.DB_PASSWORD   || 'Cisco_980';
 CONFIG.jwt_encryption  = process.env.JWT_ENCRYPTION || 'jwt_please_change';
 CONFIG.jwt_expiration  = process.env.JWT_EXPIRATION || '10000';
 
-CONFIG.database_ulr = process.env.DATABASE_URL
+CONFIG.database_url = process.env.DATABASE_URL
